42334e61f64c69796ecd3134fcd68b08dc954188
Thibaut <thibautbernard@sfr.fr>
2020-11-13 16:41:01 +0000

    git pull

---
+++ holberton.h
@@ -0,0 +1,9 @@
+#ifndef __HOLBERTON_H__
+#define __HOLBERTON_H__
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <sys/wait.h> 
+#include <stdio.h>
+#include <stdlib.h>
+void _prompt();
+#endif
