20dda5a6bc038ce826f36594ed4fc504365d6e04
Olivier <olivier.guyot.cu@gmail.com>
2020-11-14 10:58:15 +0000

    First commit

---
+++ test/t_nbwords.c
@@ -0,0 +1,28 @@
+#include "../holberton.h"
+
+int main()
+{
+	char *s = "avion:toto:bateau";
+	char *d = ":";
+	char *s1 = "avion:bateau";
+	char *s2 = "avion";
+	char *s3 = "::::::::::::::::avion::::::::::::";
+	char *s4 = ":::::::::::::::avion";
+	char *s5 = "avion::::::::::::";
+	char *s6 = "avion:bateau:::::::::::";
+	char *s7 = ":::::::::::::::avion:bateau";
+	char *s8 = ":::::::::::::::avion:bateau:::::::::::::::";
+	char *s9 = ":::::::::::::::avion::::::bateau:::::::::::::::";
+	/*      char **tb;*/
+	printf("[getnbwords] expected: 3 return : %i\n", getnbwords(s, d));
+	printf("[getnbwords] expected: 2 return : %i\n", getnbwords(s1, d));
+	printf("[getnbwords] expected: 1 return : %i\n", getnbwords(s2, d));
+	printf("[getnbwords] expected: 1 return : %i\n", getnbwords(s3, d));
+	printf("[getnbwords] expected: 1 return : %i\n", getnbwords(s4, d));
+	printf("[getnbwords] expected: 1 return : %i\n", getnbwords(s5, d));
+	printf("[getnbwords] expected: 2 return : %i\n", getnbwords(s6, d));
+	printf("[getnbwords] expected: 2 return : %i\n", getnbwords(s7, d));
+	printf("[getnbwords] expected: 2 return : %i\n", getnbwords(s8, d));
+	printf("[getnbwords] expected: 2 return : %i\n", getnbwords(s9, d));
+	return (0);
+}
