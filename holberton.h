672a943e7e52aa54d53cf5c41cc003220e097f24
Olivier <olivier.guyot.cu@gmail.com>
2020-11-18 21:49:47 +0000

    merge

--- holberton.h
+++ holberton.h
@@ -7,28 +7,46 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+extern char **environ;
+
+/*env.c*/
+int _strlen_const(const char *s);
+char *_getenv(const char *name);
+int print_env(int n);
+
+/*checkbuiltin.c*/
 typedef struct bui
 {   
 	char *name;
 	int (*f)(int);
 }bui;
-void _prompt(void);
-int _strlen(char *s);
-int _strlen_const(const char *s);
-int _putchar(char c);
-int _putstr(char *s);
-int print_env(int n);
-char *_concat(char *src, char *dest);
-char **parseintab(char *str);
-int child_process(char **argv, char **env);
-int _ischar(char c);
-int exit_process(int nb);
-int nb(char *str, char *d);
-int getnbwords(char *str, char *d);
-char *_getenv(const char *name);
-extern char **environ;
-int _strncmp(const char *s1, char *s2, int nb);
-char *parsePATH(char *cmd);
+
 int _checkbuiltin(bui *b1, char *s);
 int _launchbuiltin(bui *b1, char *s);
+
+/*_exit.c*/
+int exit_process(int nb);
+
+/*getnbwords.c*/
+int _ischar(char c);
+int getnbwords(char *str, char *d);
+
+/*parser.c*/
+char **parseintab(char *str);
+char *parsePATH(char *cmd);
+
+/*process.c*/
+int child_process(char **argv, char **env);
+int child_process_built(char **argv);
+
+/*prompt.c*/
+void _prompt(void);
+
+/*utilities.c*/
+int _strncmp(const char *s1, char *s2, int nb);
+int _strlen(char *s);
+int _putchar(char c);
+int _putstr(char *s);
+char *_concat(char *src, char *dest);
+/*shell.c MAIN*/
 #endif
