69ce4727d4cafab9376ab391137c5323f813a081
Olivier <olivier.guyot.cu@gmail.com>
2020-11-18 13:30:17 +0000

    [freetab] : add function

---
+++ utilitiesfree.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * freetab - free a char **;
+ * @tb: table to free
+ * Return: 0;
+ */
+void freetab(char **tb)
+{
+	int j;
+
+	if (tb == NULL)
+	{
+		_putstr("[freetab] can't free : tab is NULL b is free\n");
+		return (-1);
+	}
+	for (j = 0; tb[j]; j++)
+	{
+		if (tb[j] == NULL)
+		{
+			_putstr("[freetab] can't free: line is NULL\n");
+			return (-1);
+		}
+		free(tb[j]);
+	}
+	free(tb);
+	return (0);
+}
