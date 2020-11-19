75db467c5f674ab40859c2b01ec0588586834b7f
Thibaut <thibautbernard@sfr.fr>
2020-11-19 18:08:10 +0000

    merge

--- holberton.h
+++ holberton.h
@@ -7,29 +7,48 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+/*
+typedef struct env
+{   
+	char *str;
+	struct env *next;
+}envNodes;*/
 typedef struct bui
 {   
 	char *name;
 	int (*f)(int);
+	//int (*f)(int, char **, envNodes **);
 }bui;
-void _prompt(void);
+
+void _prompt(); // 
+//void _prompt(envNodes *env);
 int _strlen(char *s);
 int _strlen_const(const char *s);
 int _putchar(char c);
 int _putstr(char *s);
-int print_env(int n);
-char *_concat(char *src, char *dest);
+int print_env(int n); //
+//int print_env(int n, char **s, envNodes **env);
+char *_concat(char *src, char *dest, char delim);
 char **parseintab(char *str);
-char *_child_process(char **argv, char **env);
+char *_child_process(char **argv, char **env);//
+//char *_child_process(char **argv, char **env, envNodes *environ);
 int _ischar(char c);
-int exit_process(int nb);
+//int exit_process(int nb, char **s, envNodes **env);
+int exit_process(int nb);//
 int nb(char *str, char *d);
 int getnbwords(char *str, char *d);
-char *_getenv(const char *name);
+char *_getenv(const char *name); //
+//char *_getenv(const char *name, envNodes *env);
 extern char **environ;
 int _strncmp(const char *s1, char *s2, int nb);
-char *parsePATH(char *cmd);
+char *parsePATH(char *cmd); //
+//char *parsePATH(char *cmd, envNodes *env);
 int _checkbuiltin(bui *b1, char *s);
-int _launchbuiltin(bui *b1, char *s);
+int _launchbuiltin(bui *b1, char *s);//
+//int _launchbuiltin(bui *b1, char **s, envNodes *env);
 int freetab(char **tb);
+//envNodes *transformEnv(void);
+//envNodes *add_nodechar_end(envNodes **head, char *s);
+int _setenv(int n, char **s); //
+//int _setenv(int n, char **s, envNodes **env);
 #endif
