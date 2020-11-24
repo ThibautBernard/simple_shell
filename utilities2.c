e6d46615c7ca0d42dfc6606b72d3addd0385f192
Olivier <olivier.guyot.cu@gmail.com>
2020-11-24 13:38:19 +0000

    up utilities2

--- utilities2.c
+++ utilities2.c
@@ -18,3 +18,29 @@ char *_strdup(char *s)
 	tmp[i] = '\0';
 	return (tmp);
 }
+/**
+ *_mconcat - concatain source with destination
+ * @src: source
+ * @dest: destination
+ * Return: resultat
+ */
+
+char *_mconcat(char *src, char *dest)
+{
+	int i;
+	int j;
+	char *res;
+
+	res = malloc(sizeof(char) * (_strlen(src) + _strlen(dest) + 1));
+	if (res == NULL)
+		return (0);
+	for (i = 0; src[i]; i++)
+		res[i] = src[i];
+	for (j = 0; dest[j]; j++)
+	{   
+		res[i] = dest[j];
+		i++;
+	}   
+	res[i] = '\0';
+	return (res);
+}
