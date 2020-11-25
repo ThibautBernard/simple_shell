fdb3265cb78f4459e61311c08a1c29d56a66eb16
Olivier <olivier.guyot.cu@gmail.com>
2020-11-25 20:40:06 +0000

    test checker

--- process.c
+++ process.c
@@ -9,38 +9,40 @@
 char *_child_process(char **argv, char **env, envNodes *envi)
 {
 	char *s, *s1, *path;
-	int child, status;
+	int child, status, b;
 	struct stat *sb;
 	(void)env;
 
-	sb = malloc(sizeof(struct stat));
-	if (sb == NULL)
-		return (NULL);
 	child = fork();
 	if (child == -1)
 		perror("Error chill process");
 	if (child == 0)
 	{
+		if (argv[0] == NULL)
+		{
+			kill(child, SIGKILL);
+			/*return (NULL);*/
+		}
+		sb = malloc(sizeof(struct stat));
+		if (sb == NULL)
+			return (NULL);
+		b = stat(argv[0], sb);
+		free(sb);
 		path = checkPath(argv[0], &envi);
-/*		printf("%s\n", path);*/
 		if (path != NULL)
 			exec(path, argv, status);
-		else if (stat(argv[0], sb) == 0)
+		else if (b  == 0)
 			exec(argv[0], argv, status);
 		else if (path == NULL)
 		{
 			s = _getenv("PWD", &envi);
 			s1 = _concat(s, argv[0], '/');
-/**			free(s); **/
 			free(argv[0]);
 			argv[0] = _strdup(s1);
 			exec(s1, argv, status);
-/**		free(s1); **/
 		}
 	}
 	else
 		wait(&status);
-/**	free(path); **/
-	free(sb);
 	return (path);
 }
