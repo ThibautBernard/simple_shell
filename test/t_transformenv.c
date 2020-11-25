4a419fb55dbec039f997b199b7fa8e8c2721e901
Olivier <olivier.guyot.cu@gmail.com>
2020-11-25 11:16:40 +0000

    transforme

--- test/t_transformenv.c
+++ test/t_transformenv.c
@@ -1,9 +1,30 @@
 #include "../holberton.h"
+size_t print_list(envNodes *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%i] %s\n",i, h->str);
+		else
+			printf("[0] (nil)\n");
+
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
 
 int main()
 {
 	envNodes *envt;
-
+	
 	envt = transformEnv();
+	print_list(envt);
+	printf("***************FREE************");
 	free_list(envt);
+	printf("**************FREE*************");
+
+	return(0);
 }
